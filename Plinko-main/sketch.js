24cc94aebbb646b1bd1f5f20a9a117ea996a77b9
NeehaRampilla <70782003+NeehaRampilla@users.noreply.github.com>
2021-03-02 17:48:56 -0800

    Add files via upload

--- Plinko-main/sketch.js
+++ Plinko-main/sketch.js
@@ -6,8 +6,6 @@ var engine, world;
 var particle = [];
 var plinko = [];
 var divisions = [];
-var plinkos, score=0, count=0, gameState="start";
-
 function setup() {
   createCanvas(800,800);
   engine = Engine.create();
@@ -37,58 +35,18 @@ function setup() {
 
 function draw() {
   background(0,0,0);  
- textSize(35);
- text("score: "+score,20,40);
- fill (255);
- text(" 500 ",5,550);
- text(" 500 ",160,550);
- text(" 500 ",80,550);
- text(" 500 ",240,550);
- text(" 100 ",320,550);
- text(" 100 ",400,550);
- text(" 100 ",480,550);
- text(" 200 ",560,550);
- text(" 200 ",640,550);
- text(" 200 ",720,550);
 Engine.update(engine);
 ground.display();
-if(gameState == "end"){
-  textSize (100);
-  text ("game over",150,250);
-
-}
 for(var i = 0;i<particle.length;i++){
   particle[i].display();
 }
-
+if(frameCount%60 == 0){
+  plinko.push(new Plinko(random(width/2-30,width/2+30),10));
+}
+for(var i = 0;i<plinko.length;i++){
+  plinko[i].display();
+}
 for(var i = 0;i<divisions.length;i++){
   divisions[i].display();
 }
- if (plinkos != null){
-   plinkos.display();
-   if (plinkos.body.position.y>760){
-     if(plinkos.body.position.x<300){
-       score+=500;
-       plinkos=null; 
-     }
-     if(plinkos.body.position.x>301 && plinkos.body.position.x <600){
-      score+=100;
-      plinkos=null; 
-    } 
-    if(plinkos.body.position.x>601 && plinkos.body.position.x <900){
-      score+=500;
-      plinkos=null; 
-    }
-     if (count>= 5){
-       gameState="end";
-     }
-   }
-   
- }
-}
-function mouseReleased(){
-  if(gameState!="end"){
-    count++;
-    plinkos= new Plinko(mouseX,10)
-  }
 }
